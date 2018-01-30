6e14788a7d3fabb78317114cbb1cf1862f6302fd
Pekka Piispanen <pekka.piispanen@gmail.com>
2018-01-30 15:31:09 +0200

    Added new test file for script tags. Finished newsletter tests.

--- tests/test_client_newsletter.py
+++ tests/test_client_newsletter.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import os
+import uuid
 
 # import the RestClient class
 from epages_client.client import RestClient
@@ -26,10 +27,37 @@ class TestNewsletterMethods(BaseUnitTest):
 
     def test_001_get_newsletter_campaigns(self):
 
-        newsletter_campaigns = self.client.get_newsletter_campaigns(self.params)
+        newsletter_campaigns = self.client.get_newsletter_campaigns(
+            self.params)
 
         self.assertEqual(isinstance(newsletter_campaigns, dict), True)
 
-    def test_002_get_newsletter_campaign_subscribers(self):
+    def test_002_get_newsletter_campaign_subscribers_no_id(self):
 
-        newsletter_subscribers = self.client.get_newsletter_campaign_subscribers(self.params)
+        with self.assertRaises(ValueError) as e:
+            newsletter_subscribers = self.client.get_newsletter_campaign_subscribers(
+                self.params)
+
+    def test_003_get_newsletter_campaign_subscribers_false_id(self):
+
+        self.params["param1"] = str(uuid.uuid4())
+
+        with self.assertRaises(RuntimeError) as e:
+            newsletter_subscribers = self.client.get_newsletter_campaign_subscribers(
+                self.params)
+
+    def test_004_get_newsletter_campaign_subscribers(self):
+
+        newsletter_campaigns = self.client.get_newsletter_campaigns(
+            self.params)
+
+        # If there are some newsletters, check if the first one has subscribers
+        if newsletter_campaigns["results"] > 0:
+
+            campaign_id = newsletter_campaigns["items"][0]["campaignId"]
+            self.params["param1"] = campaign_id
+
+            newsletter_subscribers = self.client.get_newsletter_campaign_subscribers(
+                self.params)
+
+            self.assertEqual(isinstance(newsletter_subscribers, dict), True)
