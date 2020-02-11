eb1231007b49beee80ebfa1689ec73850353f6d5
Pekka Piispanen <pekka@vilkas.fi>
2020-02-11 13:16:19 +0200

    Bump version: 0.1.10 → 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='epages_client',
-    version='0.1.10',
+    version='0.2.0',
     description="Python client for ePages REST API.",
     long_description=readme + '\n\n' + history,
     author="Pekka Piispanen, Tero Kotti",
